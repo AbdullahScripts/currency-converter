15debcd2e03636e1c00a30d598925b6c15a3e58b
Abdullah Mirza <111111588+DevMirxa009@users.noreply.github.com>
2024-01-23 00:47:28 +0500

    Muhammad Abdullah:by default slect value in dropdowns

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr/pkr.json";
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr/pkr.json";
 
-let dropdowns=document.querySelectorAll(".dropdown select")
+let dropdowns = document.querySelectorAll(".dropdown select"); // dropdown selected
+
+let imgFrom = document.querySelector(".from img"); // from class image get
+let imgTo = document.querySelector(".to img"); // to class image get
+
+for (select of dropdowns) {
+  for (code in countryList) {
+    let option = document.createElement("option");
+    option.value = countryList[code];
+    option.innerText = code;
+
+    if (select.name === "from" && option.value === "US") {
+      option.setAttribute("selected", "selected");
+    } else if (select.name === "to" && option.value === "IN") {
+      option.setAttribute("selected", "selected");
+    }
+
+    select.appendChild(option);
+  }
+}
 
 
 
 
 
 
-  // let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-  
+
+
+
+
+
+// let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
