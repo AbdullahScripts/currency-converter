7699872f0aa19aa846ded21ef5026182759a0b0d
Abdullah Mirza <111111588+DevMirxa009@users.noreply.github.com>
2024-01-22 15:33:32 +0500

    Muhammad Abdullah: frontend complete

---
+++ app.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr/pkr.json";
+
+let dropdowns=document.querySelectorAll(".dropdown select")
+
+
+
+
+
+
+  // let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  
